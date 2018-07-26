97cd9cd6a13671c951f9f29ebeafd4f8703e2e15
Karen Kraft <kraftkj@comcast.net>
2018-07-26 20:29:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # dog.rb
+class Dogs
+  def initialize(name)
+    @name = name 
+  end
+  
+  def return_name
+    @name
+  end 
+end 
+
+fido = Dogs.new("Fido")
+snoopie = Dogs.new("Snoopie")
+lassie = Dogs.new("Lassie")
+
+puts fido.return_name
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # person.rb
+class Person 
+  def initialize(person)
+    @person = person
+  end 
+  def return_person 
+    @person 
+  end
+end 
+
+adele_goldberg = Person.new("Adele Goldberg")
+alan_kay = Person.new("Alan Kay")
+ 
+ puts adele_goldberg.return_person 
+    
+    
+  
